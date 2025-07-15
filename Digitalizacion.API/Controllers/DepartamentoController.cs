87e537a6e07568d1e90f3118cee752b7ac586c1c
GeorgeAngelQ <quispemescuag@gmail.com>
2025-07-14 21:23:51 -0500

    Implementacion en los métodos del controlador Departamento para la API

--- Digitalizacion.API/Controllers/DepartamentoController.cs
+++ Digitalizacion.API/Controllers/DepartamentoController.cs
@@ -52,5 +52,44 @@ namespace Digitalizacion.API.Controllers
             var blDepartamento = new DepartamentoLN();
             blDepartamento.Delete(idDepartamento);
         }
+        [HttpGet]
+        [Route("list")]
+        public IActionResult List()
+        {
+            try
+            {
+                var blDepartamento = new DepartamentoLN();
+                var list = blDepartamento.List();
+
+                if (list == null || !list.Any())
+                    return NotFound("No se encontraron departamentos");
+
+                return Ok(list);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Error interno del servidor: {ex.Message}");
+            }
+        }
+        [HttpGet]
+        [Route("pagination")]
+        public ActionResult<List<ExpandoObject>> Pagination(
+            [FromQuery] string? texto,
+            [FromQuery] int pageSize = 5,
+            [FromQuery] int currentPage = 1,
+            [FromQuery] string orderBy = "IdDepartamento",
+            [FromQuery] bool? sortOrder = true)
+        {
+            try
+            {
+                var departamentoLN = new DepartamentoLN();
+                var data = departamentoLN.Pagination(texto ?? "", pageSize, currentPage, orderBy, sortOrder);
+                return Ok(data);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { mensaje = "Error en el servidor", detalle = ex.Message });
+            }
+        }
     }
 }
